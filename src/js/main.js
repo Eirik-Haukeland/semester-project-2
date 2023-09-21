8d668ebfaf047abc88d890442414921392a5a86f
Eirik Berget Haukeland <eirik@haukeland.name>
2023-09-22 01:19:37 +0200

    open modal with js

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,32 @@ search();
 
 setInterval(() => {
   const isLoggedIn = localStorage.getItem('accessToken') !== null
+  const authForm = document.getElementById('auth-action-form')
+  const userMenu = document.getElementById('user-menu')
 
-  if (!isLoggedIn) {
+  if (!isLoggedIn && authForm === null) {
     loginRegister()
+    const heroButtons = document.querySelectorAll('#hero-section button, header button')
+    heroButtons.forEach(button => {
+      console.log(button, button.innerText)
+
+      button.addEventListener('click', (evt) => {
+        const btn = evt.target;
+        const btnText = btn.innerText.toLowerCase()
+
+        if (btnText === 'login') {
+          document.getElementById('auth-action-login').checked = true
+        }
+        if (btnText === 'register') {
+          document.getElementById('auth-action-register').checked = true
+        }
+
+        userMenu.showModal()
+      })
+    })
+  }
+
+  if (isLoggedIn) {
   }
 
 
